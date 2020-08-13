1b689a560f527a9a63c131b626d2881f7e7ce75c
Philip <me@fippa.co.uk>
2020-08-13 11:54:50 +0100

    Initial commit for the vending machine console app.

---
+++ VendingMachine/UI/AcmeSodaConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AcmeSodaConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
