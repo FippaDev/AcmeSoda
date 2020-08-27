dc1f8aa8aa7aae0f7ea2af8a6842ed0e93aab70c
Philip <me@fippa.co.uk>
2020-08-27 16:40:05 +0100

    Renaming to CashFloatTests.

--- Fippa/Fippa.Money.Tests/Payments/CashFloatTests.cs
+++ Fippa/Fippa.Money.Tests/Payments/CashFloatTests.cs
@@ -3,12 +3,21 @@ using Fippa.Money.Currencies;
 using Fippa.Money.Payments;
 using Xunit;
 
-namespace Fippa.Money.Tests.Payment
+namespace Fippa.Money.Tests.Payments
 {
     [ExcludeFromCodeCoverage]
     public class ChangeCalculatorTests
     {
         private readonly ushort MaxCoinsPerDenomination = 100;
+
+        [Fact]
+        public void Change_WhenEmptyFloat_ReturnsEmptyResult()
+        {
+            var cashFloat = new CashFloat<GBPCoins>(MaxCoinsPerDenomination);
+            var change = cashFloat.CalculateChangeToReturnToCustomer(0.99m);
+
+            Assert.Empty(change);
+        }
 
         [Fact]
         public void AddCoinsToCashFloat_GivenTenPencePieces_BalanceReflectsTheCoinsAdded()
