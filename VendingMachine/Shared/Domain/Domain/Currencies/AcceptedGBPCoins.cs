4b6019d0a8d218ba33c5a5566649dbc147b94dfe
Philip <me@fippa.co.uk>
2020-08-27 17:08:53 +0100

    Moving files about.

---
+++ VendingMachine/Shared/Domain/Domain/Currencies/AcceptedGBPCoins.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Fippa.Money.Currencies;
+using Fippa.Money.Payments;
+
+namespace Domain.Currencies
+{
+    public sealed class AcceptedGBPCoins : ICurrency
+    {
+        IEnumerable<ICashPayment> ICurrency.Collection()
+        {
+            return
+                new[]
+                {
+                    GBP.FivePence,
+                    GBP.TenPence,
+                    GBP.TwentyPence,
+                    GBP.FiftyPence,
+                    GBP.OnePound,
+                    GBP.TwoPound
+                };
+        }
+    }
+}
