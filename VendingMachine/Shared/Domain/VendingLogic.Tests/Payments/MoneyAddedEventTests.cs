1cd23dcc7a1184178655cd6fe50ff44a0e3d6998
Philip <me@fippa.co.uk>
2020-09-10 17:40:22 +0100

    Adding unit tests for MoneyAddedEvent.

---
+++ VendingMachine/Shared/Domain/VendingLogic.Tests/Payments/MoneyAddedEventTests.cs
@@ -0,0 +1,18 @@
+﻿using VendingLogic.Payments;
+using Xunit;
+
+namespace VendingLogic.Tests.Payments
+{ 
+    public class MoneyAddedEventTests
+    {
+        [Fact]
+        public void MoneyAddedEventTests_GivenItem_SetsProperty()
+        {
+            var amount = 0.79m;
+
+            var moneyAddedEvent = new MoneyAddedEvent(amount);
+
+            Assert.Equal(amount, moneyAddedEvent.Amount);
+        }
+    }
+}
