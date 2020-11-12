4580bb615ffb86a51b076e2906e208f9fe69fabe
Philip <me@fippa.co.uk>
2020-11-12 23:42:51 +0000

    New guard to only allow negative numbers.

---
+++ Fippa/Fippa.Common/GuardClauses/PositiveOrZero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Ardalis.GuardClauses;
+
+namespace Fippa.Common.GuardClauses
+{
+    // Using the same namespace will make sure your code picks up your 
+    // extensions no matter where they are in your codebase.
+    namespace Ardalis.GuardClauses
+    {
+        public static class PositiveOrZeroGuard
+        {
+            /// <summary>
+            /// Throws an exception is the input is false
+            /// </summary>
+            public static void PositiveOrZero(this IGuardClause guardClause, decimal input, string parameterName)
+            {
+                Guard.Against.Null(guardClause, nameof(guardClause));
+
+                if (input >= 0)
+                {
+                    throw new ArgumentException("Input must be negative", parameterName);
+                }
+            }
+        }
+    }
+}
