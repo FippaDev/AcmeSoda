25e56ff2c3056e7f11763867d6e7ed739115a814
Philip <me@fippa.co.uk>
2020-11-20 17:59:09 +0000

    Unit tests for decimal and string extensions.

---
+++ Fippa/Fippa.Common.Tests/Extensions/DecimalExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using Fippa.Common.Extensions.system;
+using FluentAssertions;
+using Xunit;
+
+namespace Fippa.Common.Tests.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData(false, null)]
+        [InlineData(false, "")]
+        [InlineData(false, " ")]
+        [InlineData(true, "!")]
+        public void HasValue_Scenarios_AsExpected(bool expected, string given)
+        {
+            expected.Should().Be(given.HasValue(), $"Expected {expected} when given '{given}'");
+        }
+    }
+}
---
+++ Fippa/Fippa.Common.Tests/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,17 @@
+﻿using Fippa.Common.Extensions.system;
+using Xunit;
+
+namespace Fippa.Common.Tests.Extensions
+{
+    public class DecimalExtensionsTests
+    {
+        [Theory]
+        [InlineData(false, -1.00)]
+        [InlineData(false, 0.00)]
+        [InlineData(true, 1.01)]
+        public void GreaterThanZero_Scenarios_AsExpected(bool expected, decimal given)
+        {
+            Assert.Equal(expected, given.GreaterThanZero());
+        }
+    }
+}
